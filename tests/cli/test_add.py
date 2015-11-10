b311f795f0e91b66a5c3c815009949240ea11c06
Elmir Jagudin <elmir@unity3d.com>
2015-11-10 07:14:08 +0100

    test publishing PE files with 'symstore' command

--- tests/cli/test_add.py
+++ tests/cli/test_add.py
@@ -20,3 +20,12 @@ class TestExistingStore(util.CliTester):
         self.run_add_command(["--product", "dummylib"],
                              ["dummylib.pdb"])
         self.assertSymstoreDir("existing_store.zip")
+
+
+class TestPublishPE(util.CliTester):
+    initial_dir_zip = "existing_store.zip"
+
+    def test_add_pdb(self):
+        self.run_add_command(["--product", "peprods", "--version", "1.0.1"],
+                             ["dummyprog.exe", "dummylib.dll"])
+        self.assertSymstoreDir("pe_store.zip")
