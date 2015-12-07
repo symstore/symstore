7ef25579fde345bc32471aaf145972199d65406f
Elmir Jagudin <elmir@unity3d.com>
2015-12-07 13:47:47 +0100

    use absolute imports
    make import python 3 compatible

--- symstore/__init__.py
+++ symstore/__init__.py
@@ -1,7 +1,9 @@
-from symstore import Store
-from symstore import Transactions
-from symstore import History
-from symstore import Transaction
-from version import VERSION
+from __future__ import absolute_import
+
+from symstore.symstore import Store
+from symstore.symstore import Transactions
+from symstore.symstore import History
+from symstore.symstore import Transaction
+from symstore.version import VERSION
 
 __all__ = ["Store", "Transactions", "History", "Transaction", "VERSION"]
