91cb0c9291cc5e699e659afe14408717c5b78cf6
Roland Bosa <rbosa@logitech.com>
2016-08-17 18:31:07 -0700

    Implemented multi-page reads

--- symstore/pdb.py
+++ symstore/pdb.py
@@ -65,8 +65,6 @@ class Root:
 
         :return: root bytes as an bytes array
         """
-        assert length < self.page_size, \
-            "multi-page reads not supported"
 
         start_page = start // self.page_size
         start_byte = start % self.page_size
@@ -74,11 +72,18 @@ class Root:
         end = start + length - 1
         end_page = end // self.page_size
 
-        assert start_page == end_page, \
-            "reads spanning two pages not implemented"
-
         self._seek(start_page, start_byte)
-        return self.fp.read(length)
+
+        partial_size = min(length, self.page_size - start_byte)
+        result = self.fp.read(partial_size)
+        length -= partial_size
+        while 0 < length:
+            start_page += 1
+            self._seek(start_page, 0)
+            partial_size = min(self.page_size, length)
+            result += self.fp.read(partial_size)
+            length -= partial_size
+        return result
 
     def num_streams(self):
         """
