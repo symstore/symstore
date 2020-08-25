4d3a66c44d40146469df86475656b0e953533df6
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2020-08-25 08:59:01 +0200

    mark python 3.8 as supported

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     license="MIT",
     keywords="development symstore pdb",
