a471b7f5d00d1b1c1c61f25a45b328e454e0a027
Frans J Elliott <frans.elliott@seagate.com>
2021-05-28 19:03:11 -0500

    Now using makecab.exe to compress files on Windows OSes.
    Support compression on Windows OSes, by invoking the standard
    'makecab' utility.
    
    The 'makecab' is always available with system default PATH settings.

--- symstore/cab.py
+++ symstore/cab.py
@@ -1,32 +1,42 @@
-from os import path
+import os
 
 # this flag is false on systems that don't provide gcab library
 compression_supported = True
 
-try:
-    import gi
-    gi.require_version('GCab', '1.0')
-    from gi.repository import GCab
-    from gi.repository import Gio
-except ValueError:
-    compression_supported = False
-except ImportError:
-    compression_supported = False
+if os.name == 'nt':
+    # Use built-in makecab CLI utility which is included in all Windows OSes
+    import subprocess
+else:
+    try:
+        import gi
+        gi.require_version('GCab', '1.0')
+        from gi.repository import GCab
+        from gi.repository import Gio
+    except ValueError:
+        compression_supported = False
+    except ImportError:
+        compression_supported = False
 
 
 def compress(src_path, dest_path):
     assert compression_supported
-    cab_file = GCab.File.new_with_file(path.basename(src_path),
-                                       Gio.File.new_for_path(src_path))
+    if os.name == 'nt':
+        args = ['makecab', '/D', 'CompressionType=LZX',
+                '/D', 'CompressionMemory=21', src_path, dest_path]
+        subprocess.run(args, check=True, capture_output=True)
+    else:
+        cab_file = GCab.File.new_with_file(os.path.basename(src_path),
+                                           Gio.File.new_for_path(src_path))
 
-    cab_folder = GCab.Folder.new(GCab.Compression.MSZIP)
-    cab_folder.add_file(cab_file, False)
+        cab_folder = GCab.Folder.new(GCab.Compression.MSZIP)
+        cab_folder.add_file(cab_file, False)
 
-    cab = GCab.Cabinet.new()
-    cab.add_folder(cab_folder)
+        cab = GCab.Cabinet.new()
+        cab.add_folder(cab_folder)
 
-    out_file = Gio.File.new_for_path(dest_path)
-    cab.write_simple(out_file.replace(None,
-                                      False,
-                                      Gio.FileCreateFlags.REPLACE_DESTINATION,
-                                      None))
+        out_file = Gio.File.new_for_path(dest_path)
+        cab.write_simple(
+                out_file.replace(None,
+                                 False,
+                                 Gio.FileCreateFlags.REPLACE_DESTINATION,
+                                 None))
