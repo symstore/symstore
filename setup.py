9b33d90a58c2fbd48e0ed85304d167ba8f31d12f
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2022-02-17 08:24:32 +0100

    mark python 3.10 as supported

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     license="MIT",
     keywords="development symstore pdb",
